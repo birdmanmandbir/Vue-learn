79f46151af75b97a4bd9edb83b70bf43a4e5f2fe
neil <1522674529@qq.com>
2021-12-26 20:58:04 +0800

    vue vr 测试

---
+++ src/components/VueVR.vue
@@ -0,0 +1,15 @@
+<template>
+  <Pano source="pano.jpg"></Pano>
+</template>
+<script>
+import { Pano } from "vuejs-vr";
+export default {
+  components: { Pano },
+};
+</script>
+
+<style scoped>
+body {
+  background: red;
+}
+</style>
