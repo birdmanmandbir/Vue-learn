a9ea068e6105ea99e71bb5d8a95ed81cccd1f90d
neil <1522674529@qq.com>
2021-12-26 22:39:01 +0800

    初步完成，本地图片无法加载

--- src/components/VueVR.vue
+++ src/components/VueVR.vue
@@ -1,15 +1,35 @@
 <template>
-  <Pano source="pano.jpg"></Pano>
+  <div class="vr-container">
+    <Pano
+      :source="url"
+      class="pano"
+    ></Pano>
+    <!-- <img src="../assets/pano.png" alt="pano" /> -->
+  </div>
 </template>
 <script>
 import { Pano } from "vuejs-vr";
 export default {
   components: { Pano },
+  data() {
+    return {
+      url: "../assets/pano.png",
+    //   url: "https://t4.ftcdn.net/jpg/02/09/58/11/360_F_209581186_UIHxT8WvyXZbTbHnNeEu4SFRQ3TdqBHA.jpg",
+    };
+  },
 };
 </script>
 
 <style scoped>
 body {
-  background: red;
+  height: 1080px;
+}
+.vr-container {
+    height: 100vh;
+    width: 100vw;
+}
+.pano {
+    height: 80%;
+    width: 80%;
 }
 </style>
